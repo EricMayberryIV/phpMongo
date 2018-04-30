bb3fec280e7b7ffac9222686c2a8e75eb2c80173
Eric Mayberry <eric1.mayberry@famu.edu>
2018-04-30 19:48:07 +0000

    playing with queries

--- queries.js
+++ queries.js
@@ -18,11 +18,94 @@ db.vinyl.count()
  */ 
 db.vinyl.find({
     "tracks": { $ne: null }
-    },
-    {
-        "_id": 0,
-        "artist": 1,
-        "title": 1,
-        "tracks": 1
+}, {
+    "_id" : 0,
+    "artist" : 1,
+    "title" : 1,
+    "tracks" : 1
+}).pretty()
+
+/**
+ * Records that have members
+ * Where members != null
+ */ 
+db.vinyl.find({
+    "members": { $ne: null }
+}, {
+    "_id" : 0,
+    "artist" : 1,
+    "title" : 1,
+    "members" : 1
+}).pretty()
+
+/**
+ * List all 7" records
+ */
+db.vinyl.find({
+    "size":7
+},{_
+   "_id" : 0,
+   "title" : 1,
+   "artist" : 1,
+   "size" : 1}).pretty()
+
+/**
+ * List all records which were released since 1990
+ */
+db.vinyl.find({
+    "year" : { 
+        $gte : 1990
     }
-).pretty()
+}, { 
+    "_id" : 0, 
+    "title" : 1, 
+    "artist" : 1, 
+    "year" : 1 
+}).sort({
+    "year" : 1
+}).pretty()
+
+/**
+ * List all records which were released before 1995
+ */
+db.vinyl.find({
+    "year" : { 
+        $lte : 1995
+    }
+}, { 
+    "_id" : 0, 
+    "title" : 1, 
+    "artist" : 1, 
+    "year" : 1 
+}).sort({
+    "year" : 1
+}).pretty()
+
+/**
+ * List all records which label's name start with 'Colum'
+ */
+db.vinyl.find({
+    "label" : {
+        "$regex" : /^Colum/
+    }
+},{ 
+    "_id" : 0, 
+    "title" : 1, 
+    "artist" : 1, 
+    "year" : 1
+}).pretty()
+
+/** 
+ * Nirvana albums released before 1992
+ */
+db.vinyl.find({
+    "artist" : "Nirvana",
+    "year" : {
+        $lte : 1992
+    }
+},{ 
+    "_id" : 0, 
+    "title" : 1, 
+    "artist" : 1, 
+    "year" : 1
+}).pretty()
